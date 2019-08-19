731a6f4f5bfbe0a72f667fe4a279d97b91d5b67c
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-19 11:45:23 +0700

    Update create-services.sh

--- deploy-ecs/ecs-cli/create-services.sh
+++ deploy-ecs/ecs-cli/create-services.sh
@@ -1,3 +1,3 @@
-ecs-cli compose --flie ./docker-compose-skillustrator-demo.yml --project-name skillustrator service up
+ecs-cli compose --file ./docker-compose-skillustrator-demo.yml --project-name skillustrator service up
 
 ecs-cli compose --verbose --file ./docker-compose-skillustrator-demo.yml service scale 1
